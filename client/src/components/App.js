81dc79bea3d6cfb55a61eab826f83123cdd1b9dc
Kim Brown <kbrownc341@gmail.com>
2022-11-14 11:02:27 -0500

    Added ability to add contacts

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,12 +2,20 @@ import React, { useEffect } from 'react';
 import Login from './Login';
 import Dashboard from './Dashboard';
 import useLocalStorage from '../hooks/useLocalStorage';
+import { ContactsProvider } from '../contexts/ContactsProvider';
 //import io from 'socket.io-client';
 
 function App() {
   const [id, setId] = useLocalStorage('id');
+
+  const dashboard = (
+      <ContactsProvider>
+        <Dashboard id={id} />
+      </ContactsProvider>
+    )
+
   return (
-    id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />
+    id ? dashboard : <Login onIdSubmit={setId} />
   )
 };
 
---
+++ client/src/components/NewConversationModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function NewConversationModal() {
+  
+  return (
+    <div>
+      NewConversationModal
+    </div>
+  )
+};
+
+export default NewConversationModal;
